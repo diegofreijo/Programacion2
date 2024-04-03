38d5d23fa2b5bbb60ab4bff1ef984a7919372639
Diego Freijo <giga.freijo@gmail.com>
2024-04-03 17:26:49 -0300

    clase 02: lo visto en clase

---
+++ 02.csharp_1/clase/Program.cs
@@ -0,0 +1,18 @@
+﻿// int Sumar(int a, int b)
+// {
+//     return a + b;
+// }
+
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine(Sumar(2, 3));
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // p1.Main.Correr();
+        p2.Main.Correr();
+        // p3_3.Main.Correr();
+        // p4.Main.Correr();
+    }
+}
---
+++ 02.csharp_1/clase/p2.cs
@@ -0,0 +1,53 @@
+namespace p2
+{
+     class Arma
+    {
+        public string nombre;
+        public int danio;
+
+        public override string ToString()
+        {
+            return $"{nombre} hace {danio} de danio";
+        }
+
+        public void Daniar(Salud salud)
+        {
+            salud.Daniar(this.danio);
+        }
+    }
+
+    class Salud
+    {
+        private int valor;
+
+        public Salud(int valorInicial)
+        {
+            this.valor = valorInicial;
+        }
+
+        public void Daniar(int danio)
+        {
+            Console.WriteLine($"Me estan daniando por {danio}");
+            this.valor -= danio;
+            if (valor <= 0)
+            {
+                this.valor = 0;
+                Console.WriteLine("Me mori!");
+            }
+        }
+    }
+
+    public class Main
+    {
+        public static void Correr()
+        {
+            var espada = new Arma() { nombre = "Espada", danio = 10 };
+            // Y el valor inicial?
+            var salud = new Salud(25);
+
+            espada.Daniar(salud);
+            espada.Daniar(salud);
+            espada.Daniar(salud);
+        }
+    }
+}
