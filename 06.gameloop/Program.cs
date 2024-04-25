131b4d3b0da34726ae4d913c33c0425962fd8afe
Diego Freijo <giga.freijo@gmail.com>
2024-04-25 16:24:07 -0300

    clase 06: ej1

---
+++ 06.gameloop/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // ej1_1.Program.Correr();
+        // ej1_2.Program.Correr();
+        ej1_3.Program.Correr();
+    }
+}
