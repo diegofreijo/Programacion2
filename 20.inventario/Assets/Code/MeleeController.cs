54a29094192be04c48d2887a5d86e48343ad6798
Diego Freijo <giga.freijo@gmail.com>
2024-09-11 18:25:16 -0300

    clase 20: viendo si la animacion de axe slash de mixamo la puedo hacer andar en fps

--- 20.inventario/Assets/Code/MeleeController.cs
+++ 20.inventario/Assets/Code/MeleeController.cs
@@ -10,32 +10,8 @@ public class MeleeController : MonoBehaviour
     public Animator animator;
     public TerceraPersonaInputs input;
 
-    public Transform aimTarget;
-
     private void Update()
     {
-        MoveAimTarget();
-    }
-
-    private void MoveAimTarget()
-    {
-        // Aim
-        var mouseWorldPosition = Vector3.zero;
-
-        var screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        Ray ray = Camera.main.ScreenPointToRay(screenCenter);
-        if (Physics.Raycast(ray, out RaycastHit hit, 999f, aimColliderMask))
-        {
-            // Muevo el objetivo para que la pistola lo mire
-            aimTarget.position = hit.point;
-            mouseWorldPosition = hit.point;
-        }
-
-        // Hago que el jugador mire en la direccion del mouse en el mundo
-        var aimDirection = (mouseWorldPosition - transform.position).normalized;
-        // transform.forward = aimDirection;
-        transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 1f);
-
         // Disparo
         if (input.shoot)
         {
